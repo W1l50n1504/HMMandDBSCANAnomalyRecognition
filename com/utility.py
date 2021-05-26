70ec6150a15a4b9d2d371ee2e76b7db9f6434977
w1l50n <davide.paduanelli@gmail.com>
2021-05-26 11:35:52 +0200

    scrittura delle funzioni per la gestione dei dataset generati

--- com/utility.py
+++ com/utility.py
@@ -1,7 +1,32 @@
 """
 File contenente tutte le funzioni di utility, come lettura dei dataset, creazione di file e tutto il resto
 """
-def ujcvndsoainfo():
-    return
+import pandas as pd
+
+# directory contenente i dataset da utilizzare per l'addestramento degli agenti
+dataset = '/home/w1l50n/PycharmProjects/HMMandDBSCANAnomalyRecognition/com/dataset/'
 
 
+# funzione che apre i dataset e restituisce i riferimenti
+def openDataset(file):
+    directory = dataset + file
+    data = pd.read_csv(directory)  # inserisci indirizzo dataset
+    print(data)
+
+
+def convertDataset(file):
+    print('inizio conversione dataset')
+
+    directory = dataset + file
+    newDirectory = dataset + 'newFile.csv'
+    read_file = pd.read_csv(directory)
+    read_file.to_csv(newDirectory, index=None)
+
+
+# sezione in cui si testeranno tutte le funzioni create
+if __name__ == '__main__':
+    print('Inizio prova funzioni')
+
+    file = 'sensor_fyXVCYrYSRyGivyIc9T3hN.txt'
+    #openDataset(file)
+    convertDataset(file)
