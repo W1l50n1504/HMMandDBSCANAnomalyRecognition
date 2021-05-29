799d81e67074b19073dd69ef38cfec2c50204ccf
w1l50n <davide.paduanelli@gmail.com>
2021-05-29 11:10:37 +0200

    utility.py ultimato per il caricamento dei dati dai dataset per poi poter allenare gli Hidden Markov Models

--- com/markov.py
+++ com/markov.py
@@ -1,33 +1,64 @@
 """
 File contenente la classe riguardante il sistema degli Hidden Markov Models
+n_components=6 perché vogliamo che il sistema sia in grado di riconoscere 6 tipi di attività diversi (per ora)
+questi tipi di attività sono: scendere le scale, salire le scale, stare seduti, stare in piedi, camminare, correre
 """
-import numpy as np
-from hmmlearn import hmm
 
-np.random.seed(42)
+import numpy as np
+from hmmlearn.hmm import GaussianHMM
+from com.utility import *
 
 
 class HiddenMarkovModels():
 
-    def __init__(self):
-        #
-        self.model = hmm.GaussianHMM(n_components=3, covariance_type="full")
-        #
-        self.model.startprob_ = np.array([0.6, 0.3, 0.1])
-        #
-        self.model.transmat_ = np.array([[0.7, 0.2, 0.1], [0.3, 0.5, 0.2], [0.3, 0.3, 0.4]])
-        #
-        self.model.means_ = np.array([[0.0, 0.0], [3.0, -3.0], [5.0, 10.0]])
-        #
-        self.model.covars_ = np.tile(np.identity(2), (3, 1, 1))
-        #
-        self.X, self.Z = self.model.sample(100)
-        print('HMMs creati')
+    def __init__(self, data, nSamples):
+        # in questa maniera abbiamo deciso il tipo di hmm che si vuole utilizzare e il numero di attività che si vuole riconoscere
+        # e il numero di iterazioni che deve effettuare il sistema
+        # data e' la lista contente i dati su cui allenare la rete
 
-    def nuovaFunzione(self):
-        return "ciao"
+        self.model = GaussianHMM(n_components=6, n_iter=1000).fit(np.reshape(data, [len(data), 1]))
+
+        # classifica ogni osservazione c
+        self.hidden_states = self.model.predict(np.reshape(data, [len(data), 1]))
+
+    def fitHMM(self, data, nSamples):
+        # fit Gaussian HMM to Q
+        model = GaussianHMM(n_components=2, n_iter=1000).fit(np.reshape(data, [len(data), 1]))
+
+        # classify each observation as state 0 or 1
+        hidden_states = model.predict(np.reshape(data, [len(data), 1]))
+
+        # find parameters of Gaussian HMM
+        mus = np.array(model.means_)
+        sigmas = np.array(np.sqrt(np.array([np.diag(model.covars_[0]), np.diag(model.covars_[1])])))
+        P = np.array(model.transmat_)
+
+        # find log-likelihood of Gaussian HMM
+        logProb = model.score(np.reshape(data, [len(data), 1]))
+
+        # generate nSamples from Gaussian HMM
+        samples = model.sample(nSamples)
+
+        # re-organize mus, sigmas and P so that first row is lower mean (if not already)
+        if mus[0] > mus[1]:
+            mus = np.flipud(mus)
+            sigmas = np.flipud(sigmas)
+            P = np.fliplr(np.flipud(P))
+            hidden_states = 1 - hidden_states
+
+        return hidden_states, mus, sigmas, P, logProb, samples
+
+    # load annual flow data for the Colorado River near the Colorado/Utah state line
+    # AnnualQ = np.loadtxt('AnnualQ.txt')
+
+    # log transform the data and fit the HMM
+    # logQ = np.log(AnnualQ)
+    # hidden_states, mus, sigmas, P, logProb, samples = fitHMM(logQ, 100)
 
 
 if __name__ == '__main__':
-    hmm1 = HiddenMarkovModels()
-    print(hmm1.nuovaFunzione())
+    ds = Dataset()
+    ds.main(filename_)
+    accel = ds.retAccel()
+
+    print(accel)
--- com/utility.py
+++ com/utility.py
@@ -1,76 +1,94 @@
 """
-File contenente tutte le funzioni di utility, come lettura dei dataset, creazione di file e tutto il resto
-spiegazione nomenclatura cartelle database:
-
-    dws: walking downstairs
-    ups: walking upstairs
-    sit: sitting
-    std: s  tanding
-    wlk: walking
-    jog: jogging
-
+File contenente tutte le funzioni di utility, come lettura dei dataset, elaborazione degli stessi ed estrazione delle colonne contenenti i dati che utilizzeremo per allenare e testare gli HMM
 """
+
 import os
 import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
 
-# rendi questa variabile globale
-filename_ = 'dws_1/sub_1.csv'
+# variabile globale che indica il dataset del tipo di attività che si vuole esaminare
+testFilename_ = '/test.csv'
+trainFilename_ = '/train.csv'
+
+# serve a conoscere l'absolute path della cartella in cui si trova il file utility
+absPath_ = os.getcwd()
 
 
-class Dataset:
+# posizione delle cartelle dei vari dataset
+
+
+class Dataset():
 
     def __init__(self):
-        # serve a conoscere l'absolute path della cartella in cui si trova il file utility
-        self.abs_path = os.getcwd()
+        print('Testset caricato')
+        print(absPath_)
+        self.datasetPath = absPath_ + '/train_dataset/'
 
-        # posizione delle cartelle dei vari dataset
+        self.labelDict = {'WALKING': 0, 'WALKING_UPSTAIRS': 1, 'WALKING_DOWNSTAIRS': 2, 'SITTING': 3, 'STANDING': 4,
+                          'LAYING': 5}
 
-        self.combined_dataset = self.abs_path + '/train_dataset/A_DeviceMotion_data/'
-        self.accelerometer_dataset = self.abs_path + '/train_dataset/B_Accelerometer_data/'
-        self.gyscope_dataset = self.abs_path + '/train_dataset/C_Gyroscope_data/'
+        # carico i testset
+        self.testset = pd.read_csv(self.datasetPath + testFilename_, index_col=0)
 
-        self.df = 0
+        # carico i trainset
+        self.trainset = pd.read_csv(self.datasetPath + trainFilename_, index_col=0)
 
-    def apriDataset(self, filename, dataset):
-        # attraverso il numero passatogli  sceglie il dataset da caricare,
-        # 0 quello contenente la combinazione dei dati sul giroscopio e l'accelerometro
-        # 1 quello contenente i dati sull'accelerometro
-        # 2 quello contenente i dati sul giroscopio
-        if dataset == 0:
-            self.df = pd.read_csv(os.path.join(self.combined_dataset, filename), index_col=0)
+        # provo stampa dei testset e trainset
+        self.stampaTestset()
+        self.stampaTrainset()
 
-        elif dataset == 1:
-            self.df = pd.read_csv(os.path.join(self.accelerometer_dataset, filename), index_col=0)
-
-        elif dataset == 2:
-            self.df = pd.read_csv(os.path.join(self.gyscope_dataset, filename), index_col=0)
-
-        else:
-            print("Non è stata inserita un'opzione valida")
-
-    def stampaDataset(self):
+    def stampaTestset(self):
         # viene effettuata la stampa del dataset caricato
-        print(self.df.head())
+        print('Stampa del testset')
+        print(self.testset.head())
 
-    def produce_magnitude(self, column):
-        # crea la nuova colonna contenente il vettore risultante dei tre registrati e presenti nel sistema
+    def stampaTrainset(self):
+        # viene effettuata la stampa del dataset caricato
+        print('Stampa del trainset')
+        print(self.trainset.head())
+
+    def produceMagnitude(self, column):
+        # controllare se serve ancora o posso utilizzarla in qualche maniera
         self.df[column + '.mag'] = np.sqrt(
             self.df[column + '.x'] ** 2 + self.df[column + '.y'] ** 2 + self.df[column + '.z'] ** 2)
 
-    def main(self, filename_):
-        self.apriDataset(filename_, 0)
-        self.stampaDataset()
+    def loadTestset(self):
+        self.stampaTestset()
+        self.stampaTrainset()
 
-        self.produce_magnitude('userAcceleration')
-        self.produce_magnitude('rotationRate')
-        self.stampaDataset()
+        # sono tutti file in cui si caricheranno i dati di testset modificati
+        train_X = self.trainset.values[:, :-2]
+
+        train_y = self.trainset['Activity']
+        train_y = train_y.map(self.labelDict).values
+
+        test_X = self.testset.values[:, :-2]
+
+        test_y = self.testset
+        test_y = test_y['Activity'].map(self.labelDict).values
+
+        return train_X, train_y, test_X, test_y
+
+    def main(self):
+        print('Sto caricando i file necessari...')
+
+        # self.stampaTestset()
+        # self.stampaTrainset()
+
+        trainAndTest = self.loadTestset()
+        print('Finito!')
+
+        return trainAndTest
 
 
 # sezione in cui si testeranno tutte le funzioni create
+
+
 if __name__ == '__main__':
-    # creazione dell'oggetto che controlla il contenuto del dataset devicemotion_data
     ds = Dataset()
-    ds.main(filename_)
+    trainAndTest = ds.main()
+
+    for i in range(0, 3):
+        print(trainAndTest[i])
